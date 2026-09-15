1a8667a99e6b0613a66f5e126ba8d54a98db884d
Sardorbek_1408 <sardorergashev09uzb@gmail.com>
2026-09-15 23:20:21 +0500

    Configure permissive CORS headers for Vercel and mobile clients

--- api/config/main.php
+++ api/config/main.php
@@ -124,5 +124,13 @@ return [
     ],
     'as corsFilter' => [
         'class' => \yii\filters\Cors::class,
+        'cors' => [
+            'Origin' => ['*'],
+            'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+            'Access-Control-Request-Headers' => ['*'],
+            'Access-Control-Allow-Credentials' => null,
+            'Access-Control-Max-Age' => 86400,
+            'Access-Control-Expose-Headers' => ['*'],
+        ],
     ],
 ];
